705a97d1bc3cc875b36743c2a2227022301c1d81
PUSHPAL <pushpalroy2007@gmail.com>
2019-04-03 10:59:42 +0530

    Added Facebook authentication.

--- app/src/main/java/com/techradge/fabler/ui/activity/LoginActivity.java
+++ app/src/main/java/com/techradge/fabler/ui/activity/LoginActivity.java
@@ -42,7 +42,8 @@ public class LoginActivity extends AppCompatActivity {
         // Authentication providers
         List<AuthUI.IdpConfig> providers = Arrays.asList(
                 new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build());
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build());
 
         // Create and launch sign-in intent
         startActivityForResult(
