61279e9c6e95697ad9618d23b97eab8ecd872a78
PUSHPAL <pushpalroy2007@gmail.com>
2019-04-16 10:48:17 +0530

    Restructured Story Adapter.

--- app/src/main/java/com/techradge/fabler/ui/story/StoryAdapter.java
+++ app/src/main/java/com/techradge/fabler/ui/story/StoryAdapter.java
@@ -1,6 +1,5 @@
 package com.techradge.fabler.ui.story;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
@@ -12,9 +11,10 @@ import android.widget.TextView;
 
 import com.sackcentury.shinebuttonlib.ShineButton;
 import com.techradge.fabler.R;
+import com.techradge.fabler.data.model.Story;
 import com.techradge.fabler.data.remote.RemoteFireDatabase;
 import com.techradge.fabler.data.remote.operations.story.StoryFireOp;
-import com.techradge.fabler.data.model.Story;
+import com.techradge.fabler.ui.base.BaseViewHolder;
 import com.techradge.fabler.ui.comment.CommentActivity;
 
 import java.util.List;
@@ -22,96 +22,53 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class StoryAdapter extends RecyclerView.Adapter<StoryAdapter.StoryViewHolder> {
+public class StoryAdapter extends RecyclerView.Adapter<BaseViewHolder> {
 
-    protected Context context;
-    private List<Story> storyList;
+    private List<Story> mStoryList;
     private StoryClickListener storyClickListener;
 
-    public StoryAdapter(List<Story> storyList, Context context, StoryClickListener storyClickListener) {
-        this.context = context;
-        this.storyList = storyList;
+    public StoryAdapter(List<Story> storyList, StoryClickListener storyClickListener) {
+        this.mStoryList = storyList;
         this.storyClickListener = storyClickListener;
     }
 
     @NonNull
     @Override
     public StoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        StoryViewHolder viewHolder;
-        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_story, parent, false);
-        viewHolder = new StoryViewHolder(layoutView);
-        return viewHolder;
+        View layoutView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_story, parent, false);
+        return new StoryViewHolder(layoutView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull StoryViewHolder storyViewHolder, final int position) {
+    public void onBindViewHolder(@NonNull BaseViewHolder holder, int position) {
+        holder.onBind(position);
+    }
 
-        final Story story = storyList.get(position);
-
-        if (story.getTitle() != null)
-            storyViewHolder.title.setText(story.getTitle());
-        if (story.getTime() != null)
-            storyViewHolder.timeStamp.setText(story.getTime());
-        if (story.getAuthor() != null)
-            storyViewHolder.author.setText(story.getAuthor());
-        if (story.getStory() != null)
-            storyViewHolder.story.setText(story.getStory());
-        if (story.getLikes() != null)
-            storyViewHolder.likes.setText(story.getLikes());
-        else
-            storyViewHolder.likes.setText(context.getString(R.string.zero));
-        if (story.getComments() != null)
-            storyViewHolder.comments.setText(story.getComments());
-        else
-            storyViewHolder.comments.setText(context.getString(R.string.zero));
-
-        storyViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                storyClickListener.onStoryClick(position, story);
-            }
-        });
-
-        storyViewHolder.likeButton.setOnCheckStateChangeListener(new ShineButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(View view, boolean checked) {
-                if (checked) {
-                    StoryFireOp storyFireOp = new StoryFireOp(RemoteFireDatabase.getFirebaseDatabase(), context);
-                    storyFireOp.postLikeUpdateStory(story);
-                }
-            }
-        });
-
-        storyViewHolder.commentButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent commentIntent = new Intent(context, CommentActivity.class);
-                commentIntent.putExtra(context.getString(R.string.key_story_id), story.getStoryId());
-                commentIntent.putExtra(context.getString(R.string.key_comments), story.getComments());
-                context.startActivity(commentIntent);
-            }
-        });
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
     }
 
     @Override
     public int getItemCount() {
-        return storyList.size();
+        return mStoryList.size();
     }
 
-    static class StoryViewHolder extends RecyclerView.ViewHolder {
+    class StoryViewHolder extends BaseViewHolder {
 
         @BindView(R.id.tv_time)
-        TextView timeStamp;
+        TextView timeStampTv;
         @BindView(R.id.tv_title)
-        TextView title;
+        TextView titleTv;
         @BindView(R.id.tv_author)
-        TextView author;
+        TextView authorTv;
         @BindView(R.id.tv_story)
-        TextView story;
+        TextView storyTv;
         @BindView(R.id.tv_likes)
-        TextView likes;
+        TextView likesTv;
         @BindView(R.id.tv_comments)
-        TextView comments;
+        TextView commentsTv;
         @BindView(R.id.btn_like)
         ShineButton likeButton;
         @BindView(R.id.icon_comment)
@@ -120,6 +77,67 @@ public class StoryAdapter extends RecyclerView.Adapter<StoryAdapter.StoryViewHol
         StoryViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
+        }
+
+        @Override
+        public void onBind(final int position) {
+            super.onBind(position);
+
+            final Story story = mStoryList.get(position);
+
+            if (story.getTitle() != null)
+                titleTv.setText(story.getTitle());
+            if (story.getTime() != null)
+                timeStampTv.setText(story.getTime());
+            if (story.getAuthor() != null)
+                authorTv.setText(story.getAuthor());
+            if (story.getStory() != null)
+                storyTv.setText(story.getStory());
+            if (story.getLikes() != null)
+                likesTv.setText(story.getLikes());
+            else
+                likesTv.setText(itemView.getContext().getString(R.string.zero));
+            if (story.getComments() != null)
+                commentsTv.setText(story.getComments());
+            else
+                commentsTv.setText(itemView.getContext().getString(R.string.zero));
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    storyClickListener.onStoryClick(position, story);
+                }
+            });
+
+            likeButton.setOnCheckStateChangeListener(new ShineButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(View view, boolean checked) {
+                    if (checked) {
+                        StoryFireOp storyFireOp = new StoryFireOp(RemoteFireDatabase.getFirebaseDatabase(), itemView.getContext());
+                        storyFireOp.postLikeUpdateStory(story);
+                    }
+                }
+            });
+
+            commentButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent commentIntent = new Intent(itemView.getContext(), CommentActivity.class);
+                    commentIntent.putExtra(itemView.getContext().getString(R.string.key_story_id), story.getStoryId());
+                    commentIntent.putExtra(itemView.getContext().getString(R.string.key_comments), story.getComments());
+                    itemView.getContext().startActivity(commentIntent);
+                }
+            });
+        }
+
+        @Override
+        protected void clear() {
+            titleTv.setText("");
+            timeStampTv.setText("");
+            authorTv.setText("");
+            storyTv.setText("");
+            likesTv.setText("");
+            commentsTv.setText("");
         }
     }
 }
